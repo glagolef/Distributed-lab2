5ae97863e2f3bc1e0857974bd2a9f290a43acf18
glagolef <glagolef@tcd.ie>
2016-01-23 17:16:14 +0000

    change

--- Server.rb
+++ Server.rb
@@ -1,11 +1,17 @@
+require 'thread'
 require 'socket'
-require './Thread_Pool.rb'
-ip_addr = "0.0.0.0"
-thread_pool = Thread_Pool.new(1)
-port = ARGV[0]
-server = TCPServer.new(ip_addr, port)
-student_id=12345678
-# ip= Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
+class MultiThreadedServer
+	def initialize
+		puts "Initializing"
+		# @ip_addr = "0.0.0.0"
+		@ip_addr = '127.0.0.1'
+		@thread_pool_size = 10
+		@jobs = Queue.new
+		@port = ARGV[0]
+		@server = TCPServer.new(@ip_addr, @port)
+		@student_id=12345678
+		run
+		# ip= Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
 # puts "Listening on #{ip.ip_address}:#{port}"
 # thread_pool.add_workers(3)
 # puts thread_pool.workers_status
@@ -13,40 +19,51 @@ student_id=12345678
 # puts thread_pool.workers_status
 # thread_pool.status_of_queue
 # thread_pool.return_to_q
-loop do 
-	begin
-		vacant_workers = thread_pool.workers_status-1
-		if(vacant_workers/1.5<1)
-			add_workers(vacant_workers)
-		elsif(vacant_workers/1.5>1)
-			remove_workers(vacant_workers/2)
-		end
-			thr = thread_pool.do_work{
-				socket=server.accept; 
-				request=socket.gets;
-				if(request.contains("HELO"))
-					socket.puts("#{request}IP:#{ip_addr}\nPort:#{port}\nStudentID:#{student_id}\n")
-				elsif (request.contains("KILL_SERVICE"))
-					shutdown;
-					Thread.join;
-				else #donothing
+	end
+	def run
+		@ip_addr="134.226.44.149"
+		puts "Running"
+		kill_service = false
+		#jobs queue
+		15.times{|i| @jobs.push i} 
+		workers = (@thread_pool_size).times.map do
+			Thread.new do
+				begin
+				while x = @jobs.pop(true)
+					Thread.start(socket = @server.accept) do |client|
+						puts "Connection accepted"
+						request=socket.gets
+						puts request
+						a=manageRequest(request)
+						puts a
+						case a
+						when 1
+							client.puts "#{request}IP:#{@ip_addr}\nPort:#{@port}\nStudentID:#{@student_id}\n"							
+							client.close
+						when 0
+							client.close
+							exit
+						else
+							client.close
+						end
+					end
 				end
-				socket.close;
-				Thread.sleep}
-	rescue
-	#do nothing
+				rescue ThreadError
+				#do nothing
+				end
+			end
+	end
+	workers.map(&:join);
+
+	end
+	def manageRequest(request)
+		if request.include?"HELO"
+						return 1
+		elsif request.include?"KILL_SERVICE"
+						return 0
+		else return -1
+			puts "return null"
+		end
 	end
 end
-thr = thread_pool.do_work{puts "thread at work"; Thread.sleep}
-puts thr.status
-thread_pool.return_to_q(thr)
-puts thr.status
-# thread_pool.return_to_q(thr)
-# thr.join
-puts student_id
-thread_pool.status_of_queue
-# puts thread_pool.workers_status
-def shutdown
-	thread_pool.shutdown
-	exit
-end
+a = MultiThreadedServer.new
